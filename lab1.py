f2fb0919904d2d15df97be387cae4c7978359e3d
Andrew Glenn <andglenn@amazon.com>
2018-11-16 07:53:23 -0600

    Adding info to readme; renaming initial file to lab1

---
+++ lab1.py
@@ -0,0 +1,18 @@
+
+class UnicornRanch(object):
+    def __init__(self):
+        self.ranch_sqft = 326700
+        self.barn_sqft = 1200
+
+        sefl.watering_pond = {
+            'depth':30,
+            'shape':'circle',
+            'radi': 681
+        }
+
+        self.prop_tax_rates = {
+            'water_surface_acerage': '0.74',
+            'structure': '1.33202',
+            'grassland': '.003 per .004 acre'
+        }
+
